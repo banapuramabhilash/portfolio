4eeb0f12932ac9f18bb002b557b4650b030aa1d6
B Abhilash Reddy <banapuramabhilash@gmail.com>
2023-02-26 22:29:02 +0530

    Added lottie animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import Lottie from "lottie-react";
+import developerSkills from "./assets/lottie-animations/developer-skills.json";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="developer-skill-container">
+          <Lottie animationData={developerSkills} />
+        </div>
         <h2>B Abhilash Reddy</h2>
         <p>MEAN Stack Developer @ Araneus Solutions</p>
       </header>
